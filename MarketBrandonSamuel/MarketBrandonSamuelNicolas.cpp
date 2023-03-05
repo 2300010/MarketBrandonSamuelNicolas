469b93e44881b66297f91b0a5540676ea5b5f863
samab <samab@SAMAB>
2023-03-05 17:40:24 -0500

    Correction Hard-code Main

--- MarketBrandonSamuel/MarketBrandonSamuelNicolas.cpp
+++ MarketBrandonSamuel/MarketBrandonSamuelNicolas.cpp
@@ -11,19 +11,6 @@ int main()
     ErrorManager myErrorManager;
 	UiManager myUiManager;
 
-    // Hard coding 6 different catalog items and adding them to Warehouse
-    Item CATALOG_ITEM_1("Apple", 12, 7);
-    Item CATALOG_ITEM_2("Pineapple", 28, 15);
-    Item CATALOG_ITEM_3("Watermelon", 34, 18);
-    Item CATALOG_ITEM_4("Grape", 7, 2);
-    Item CATALOG_ITEM_5("Strawberry", 5, 3);
-    Item CATALOG_ITEM_6("Raspberry", 5, 4);
-
-    vector<Item> itemCatalog{ CATALOG_ITEM_1, CATALOG_ITEM_2, CATALOG_ITEM_3, 
-        CATALOG_ITEM_4, CATALOG_ITEM_5,CATALOG_ITEM_6 };
-
-    myWarehouse.Add_Catalog(itemCatalog);
-    //
 
 	while (true) {
 		try
